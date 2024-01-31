88b77c470ad5f632f1eaec228a4f3a07911f453d
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2024-01-31 14:43:11 +0900

    feat: jdk1.8 샘플 프로젝트 추가

---
+++ src/main/java/com/example/samplejdk1_8boot2/SampleJdk18Boot2Application.java
@@ -0,0 +1,13 @@
+package com.example.samplejdk1_8boot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJdk18Boot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleJdk18Boot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplejdk1_8boot2/SampleJdk18Boot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplejdk1_8boot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SampleJdk18Boot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
